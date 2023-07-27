4ddbe828c998edf304c7ad8fac9fb1534da53e72
swarnamalya <swarnamalya044@gmail.com>
2023-07-27 21:38:30 +0530

    first commit

---
+++ variable.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "ec2_instance" {
+  count         = var.instance_count
+  ami           = var.ami_id
+  instance_type = var.instance_type
+}
